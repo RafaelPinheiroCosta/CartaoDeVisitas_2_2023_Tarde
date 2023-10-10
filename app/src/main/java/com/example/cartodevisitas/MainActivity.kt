c23873e6a98a706ffe46202da0745df05eee99f0
Rafael Costa <rafaelcost@gmail.com>
2023-10-10 15:34:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cartodevisitas/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.example.cartodevisitas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cartodevisitas.ui.theme.CartãoDeVisitasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CartãoDeVisitasTheme {
+                CartaoDeVisitas()
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun CartaoDeVisitas() {
+   Box{
+        Image(
+            painter = painterResource(id = R.drawable.fundo),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            InformacoesPessoais()
+            InformacoesDeContato()
+        }
+    }
+}
+
+@Composable
+fun InformacoesPessoais() {
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .padding(top=180.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.foto),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .border(4.dp, Color.Black, CircleShape)
+        )
+        Text(
+            text = "Rafael Pinheiro Costa",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Professor de TI",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red
+        )
+    }
+}
+
+@Composable
+fun InformacoesDeContato() {
+    Column {
+        Contato(
+            contato = "(11) 95555-5555",
+            id = R.drawable.telefone
+        )
+        Contato(
+            contato = "xxxxxxxx@gmail.com",
+            id = R.drawable.email
+        )
+        Contato(
+            contato = "github/xxxxxxxx",
+            id = R.drawable.github
+        )
+    }
+}
+
+@Composable
+fun Contato(contato:String, id:Int) {
+    Row (
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(id = id),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Black, CircleShape)
+        )
+        Text(
+            text = contato,
+            fontSize = 20.sp
+        )
+    }
+    Spacer(modifier = Modifier.size(20.dp))
+}
